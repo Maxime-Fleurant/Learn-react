198d8b618da2f02a125f227f50757f05c38cdadc
Maxime Fleurant <maximefleurant.exe@gmail.com>
2019-01-30 20:50:50 +0100

    state and props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from "./Person/Person";
+import person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons : [
+            {
+                name: "max",
+                age : 12
+            },
+            {
+                name: "alex",
+                age : 13
+            },
+            {
+                name: "pd",
+                age : 14
+            },
+        ]
+    }
+
+    buttonHandler = () => {
+        this.setState({
+            persons : [
+                {
+                    name: "maxime",
+                    age : 12
+                },
+                {
+                    name: "alex",
+                    age : 13
+                },
+                {
+                    name: "pd",
+                    age : 17
+                },
+            ]
+        })
+
+    }
+
     render() {
         return (
             <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <Person />
+                <button onClick={this.buttonHandler}>switch</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >hello there</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 
-const person = () => {
-
-    return <p>I'm a Person!</p>
-
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm a {props.name} Person! and i'm {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 
