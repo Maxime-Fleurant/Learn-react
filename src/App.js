05e80c031a7c7c6555bdef0775931b5336edef28
Maxime Fleurant <maximefleurant.exe@gmail.com>
2019-01-30 10:16:50 +0100

    set

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <Person />
+            </div>
+        );
+    }
 }
 
 export default App;
